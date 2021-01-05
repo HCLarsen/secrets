2ea7f65ff6888cb4921ace2c1341f7574b61312f
Chris Larsen <clarsendev@protonmail.com>
2021-01-05 00:16:13 -0500

    Add generator

--- src/secrets.cr
+++ src/secrets.cr
@@ -1,15 +1,64 @@
+require "openssl"
+require "./any"
+
 # TODO: Write documentation for `Secrets`
 class Secrets
   VERSION = "0.1.0"
 
-  getter filepath
+  getter file_path : String
+  getter key_path : String
+  @data = Any.new({} of String => Any)
 
-  def initialize(filepath = "secrets.yml.enc")
-    extension = ".yml.enc"
-    if filepath.ends_with? extension
-      @filepath = filepath
-    else
-      @filepath = filepath + extension
+  def initialize(file_path = "secrets.yml.enc", key_path = "secrets.key")
+    @file_path = Secrets.path_with_extension(file_path)
+    @key_path = Secrets.key_path_with_extension(key_path)
+  end
+
+  # Generates the encrypted file and key file.
+  #
+  # If this command is run from the same directory as the `gitignore` file, it
+  # will also read the file, and add ``*.key` to it if necessary.
+  #
+  def self.generate(path = "secrets.yml.enc", key_path = "secrets.key")
+    File.new(path_with_extension(path), "w")
+    key = Secrets.generate_key
+    File.write(key_path_with_extension(key_path), key)
+
+    if File.exists?(".gitignore")
+      ignore_content = key_path_with_extension(key_path)
+      unless File.read(".gitignore").includes?(ignore_content)
+        File.write(".gitignore", ignore_content, mode: "a")
+      end
     end
+  end
+
+  delegate :[], to: @data
+  delegate :[]=, to: @data
+
+  protected def self.path_with_extension(path : String) : String
+    extension = ".yml.enc"
+    if path.ends_with? extension
+      path
+    else
+      path + extension
+    end
+  end
+
+  protected def self.key_path_with_extension(path : String) : String
+    extension = ".key"
+    if path.ends_with? extension
+      path
+    else
+      path + extension
+    end
+  end
+
+  protected def self.generate_key
+    cipher = new_cipher
+    Random::Secure.hex(cipher.key_len)[0, cipher.key_len]
+  end
+
+  private def self.new_cipher
+    OpenSSL::Cipher.new("aes-256-cbc")
   end
 end
--- test/secrets_test.cr
+++ test/secrets_test.cr
@@ -3,24 +3,75 @@ require "minitest/autorun"
 require "/../src/secrets.cr"
 
 class SecretsTest < Minitest::Test
-  def test_initializes_new_secrets_object
-    secrets = Secrets.new
-    assert_equal "secrets.yml.enc", secrets.filepath
+  @default_path = "secrets.yml.enc"
+  @default_key_path = "secrets.key"
+
+  def setup
+    if File.exists?(".gitignore")
+      File.copy(".gitignore", ".gitignore-main")
+    end
   end
 
-  def test_initializes_with_custom_path
-    secrets = Secrets.new("configs/credentials.yml.enc")
-    assert_equal "configs/credentials.yml.enc", secrets.filepath
+  def teardown
+    File.delete(@default_path) if File.exists?(@default_path)
+    File.delete(@default_key_path) if File.exists?(@default_key_path)
+
+    if File.exists?(".gitignore-main")
+      File.rename(".gitignore-main", ".gitignore")
+    end
+  end
+
+  def test_initializes_with_default_paths
+    secrets = Secrets.new
+    assert_equal @default_path, secrets.file_path
+    assert_equal @default_key_path, secrets.key_path
+  end
+
+  def test_initializes_with_custom_paths
+    secrets = Secrets.new("config/credentials.yml.enc", "config/master.key")
+    assert_equal "config/credentials.yml.enc", secrets.file_path
+    assert_equal "config/master.key", secrets.key_path
   end
 
   def test_adds_file_extension_if_not_provided
-    secrets = Secrets.new("configs/credentials")
-    assert_equal "configs/credentials.yml.enc", secrets.filepath
+    secrets = Secrets.new("config/credentials", "config/master")
+    assert_equal "config/credentials.yml.enc", secrets.file_path
+    assert_equal "config/master.key", secrets.key_path
   end
-  
+
   def test_initializes_hashlike
+    secrets = Secrets{ "username" => "warmachine68@starkindustries.com", "password" => "WARMACHINEROX" }
+    assert_equal "WARMACHINEROX", secrets["password"].as_s
+  end
+
+  def test_nested_values
+    secrets = Secrets.new
+    secrets["login"] = { "username" => Any.new("warmachine68@starkindustries.com"), "password" => Any.new("WARMACHINEROX") }
+
+    expected = { "username" => "warmachine68@starkindustries.com", "password" => "WARMACHINEROX" }
+    assert_equal expected, secrets["login"].as_h
+    assert_equal "warmachine68@starkindustries.com", secrets["login"]["username"].as_s
   end
 
   def test_raises_for_invalid_filename
+  end
+
+  def test_generates_default_files
+    Secrets.generate
+
+    assert File.exists?(@default_path)
+    assert File.exists?(@default_key_path)
+
+    key = File.read(@default_key_path)
+    assert_match /[\w\d]+/, key
+  end
+
+  def test_modifies_gitignore_file
+    File.touch(".gitignore")
+
+    Secrets.generate
+
+    gitignore = File.read(".gitignore")
+    assert_match /#{@default_key_path}/, gitignore
   end
 end
